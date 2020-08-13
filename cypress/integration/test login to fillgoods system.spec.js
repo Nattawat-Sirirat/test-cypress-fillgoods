7683320a0f336990f1a2b1774854f8a8ed47dec5
Nattawat_S <nattsu2537@gmail.com>
2020-08-13 23:59:43 +0700

    login page success

--- cypress/integration/test login to fillgoods system.spec.js
+++ cypress/integration/test login to fillgoods system.spec.js
@@ -1,20 +1,19 @@
 const url= 'https://app.fillgoods.co/login'
-const loginbtn= '//*[@id="app"]/div/div/div/div/div/div[2]/form/div/div[2]/div[1]/button'
-const username= '//*[@id="input-34"]'
-const password= '//*[@id="input-37"]'
 
 describe('login to fillgoods',() =>{
    it('visit fillgood.co', function(){
        cy.visit(url)
    })
    it('Input username', function(){
-       cy.get(username).type('nattsu2537@gmail.com')
+       cy.get('input[data-testid="emailInput"]').type('nattsu2537@gmail.com')
    })
    it('Input password', function(){
-       cy.get(password).type('nattsu20')
+       cy.get('input[data-testid="passwordInput"]').type('nattsu20')
    })
    it('Click login', function(){
-       cy.click(loginbtn)
+       cy.wait(5000)
+       cy.get('button[data-testid="loginBtn"]').click()
+       cy.wait(5000)
    })
 
 })
